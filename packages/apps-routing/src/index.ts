8e9cacc73323b02cee5e4e871da630aa209e1b80
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-08-30 11:08:29 +0300

    Boilerplate from marketplace

---
+++ packages/apps-routing/src/index.ts
@@ -0,0 +1,32 @@
+// Copyright 2017-2021 @polkadot/apps-routing, UseTech authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { TFunction } from 'i18next';
+import type { Routes } from './types';
+
+import envConfig from '@polkadot/apps-config/envConfig';
+
+import accounts from './accounts';
+import faq from './faq';
+import nftMarket from './nft-market';
+import nftTrades from './nft-trades';
+import nftWallet from './nft-wallet';
+
+const { walletMode } = envConfig;
+
+export default function create (t: TFunction): Routes {
+  if (walletMode) {
+    return [
+      nftWallet(t),
+      accounts(t)
+    ];
+  }
+
+  return [
+    nftTrades(t),
+    nftWallet(t),
+    nftMarket(t),
+    accounts(t),
+    faq(t)
+  ];
+}
