6413aeed1e3c8de8a50237b919b48aea029f4634
vaagnmazmanyan <vmazmanyanvaagn@gmail.com>
2021-11-02 17:22:18 +0400

    create 'Token Attributes' component.

--- packages/page-builder/src/components/TokenAttributes/AttributesRow.tsx
+++ packages/page-builder/src/components/TokenAttributes/AttributesRow.tsx
@@ -5,13 +5,54 @@ import './styles.scss';
 
 import React, { memo, ReactElement } from 'react';
 
+import questionIcon from '@polkadot/app-builder/images/questionIcon.svg';
+
+import trashIcon from '../../images/trashIcon.svg';
+
 function AttributesRow (): ReactElement {
   return (
-    <div className='text-field-row'>
-      <p>Name</p>
-      <p>Text</p>
-      <p>Required</p>
-      <p>Male, Female</p>
+    <div className='text-field-row '>
+      <div className='text-section'>
+        <div className='attribute-label'>
+          <p>Attribute name</p>
+          <img src={questionIcon as string} />
+        </div>
+        <div className='text-content'>
+          Name
+        </div>
+      </div>
+      <div className='text-section type'>
+        <div className='attribute-label'>
+          <p>Type</p>
+          <img src={questionIcon as string} />
+        </div>
+        <div className='text-content'>
+          Select
+        </div>
+      </div>
+      <div className='text-section rule'>
+        <div className='attribute-label'>
+          <p>Rule</p>
+          <img src={questionIcon as string} />
+        </div>
+        <div className='text-content'>
+          Required
+        </div>
+      </div>
+      <div className='text-section'>
+        <div className='attribute-label'>
+          <p>Posaible value</p>
+          <img src={questionIcon as string} />
+        </div>
+        <div className='last-section'>
+          <div className='text-content'>
+            Black Lipstick, Red Lipstick, Smile, Teeth Smile,
+            Purple Lipstick, Nose Ring, Asian Eyes, Sunglasses,
+            Red Glasses, Round Eyes, Left Earring, Right Earring, Two Earrings, Brown Beard, Mustache Beard,
+          </div>
+          <img src={trashIcon as string} />
+        </div>
+      </div>
     </div>
   );
 }
--- packages/page-builder/src/components/TokenAttributes/index.tsx
+++ packages/page-builder/src/components/TokenAttributes/index.tsx
@@ -6,11 +6,11 @@ import './styles.scss';
 import React, { memo, ReactElement } from 'react';
 
 import AddAttributesRow from '@polkadot/app-builder/components/TokenAttributes/AddAttributesRow';
-import AttributesRow from '@polkadot/app-builder/components/TokenAttributes/AttributesRow';
 
 import infoIcon from '../../images/infoIcon.svg';
 import plusIcon from '../../images/plusIcon.svg';
 import questionIcon from '../../images/questionIcon.svg';
+import AttributesRow from './AttributesRow';
 
 function TokenAttributes (): ReactElement {
   return (
@@ -19,16 +19,26 @@ function TokenAttributes (): ReactElement {
         <p className='header-title'>Token attributes</p>
         <p className='header-text'>This functionality allows you to customize the token. You can set any traits that will help you create unique NFT: name, accessory, gender, background, face, body, tier etc.</p>
       </div>
-      <div className='section'>
+      <div className='attributes-title'>
         <div className='row-title'>
-          <div><p>Attribute</p><img src={questionIcon as string} /></div>
-          <div><p>Type</p><img src={questionIcon as string} /></div>
-          <div><p>Rule</p><img src={questionIcon as string} /></div>
-          <div><p>Possible values</p><img src={questionIcon as string} /></div>
+          <p>Attribute type</p>
+          <img src={questionIcon as string} />
         </div>
-        <AddAttributesRow />
-        <AttributesRow />
+        <div className='row-title'>
+          <p>Type</p>
+          <img src={questionIcon as string} />
+        </div>
+        <div className='row-title'>
+          <p>Rule</p>
+          <img src={questionIcon as string} />
+        </div>
+        <div className='row-title'>
+          <p>Possible values</p>
+          <img src={questionIcon as string} />
+        </div>
       </div>
+      <AttributesRow />
+      <AddAttributesRow />
       <div className='add-field'>Add field <img src={plusIcon as string} /></div>
       <div className='warning-text'>
         <img src={infoIcon as string} />
