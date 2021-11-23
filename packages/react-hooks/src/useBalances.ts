0e3a167b993ffda0a6b54d8c93e6cf5518dbacbc
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-11-23 09:16:28 +0300

    remove kusama from builder

--- packages/react-hooks/src/useBalances.ts
+++ packages/react-hooks/src/useBalances.ts
@@ -6,20 +6,16 @@ import type { DeriveBalancesAll } from '@polkadot/api-derive/types';
 import BN from 'bn.js';
 import { useEffect, useState } from 'react';
 
-import { useApi, useCall, useKusamaApi } from '@polkadot/react-hooks';
+import { useApi, useCall } from '@polkadot/react-hooks';
 
 interface UseBalancesInterface {
   freeBalance: BN | undefined;
-  freeKusamaBalance: BN | undefined;
 }
 
 export const useBalances = (account: string | undefined, getUserDeposit?: () => Promise<BN | null>): UseBalancesInterface => {
   const { api } = useApi();
-  const { kusamaApi } = useKusamaApi(account || '');
   const balancesAll = useCall<DeriveBalancesAll>(api.derive.balances?.all, [account]);
-  const kusamaBalancesAll = useCall<DeriveBalancesAll>(kusamaApi?.derive.balances?.all, [account]);
   const [freeBalance, setFreeBalance] = useState<BN>();
-  const [freeKusamaBalance, setFreeKusamaBalance] = useState<BN>();
 
   useEffect(() => {
     // available balance used as free (transferable)
@@ -29,19 +25,11 @@ export const useBalances = (account: string | undefined, getUserDeposit?: () => 
   }, [balancesAll]);
 
   useEffect(() => {
-    // available balance used as free (transferable)
-    if (kusamaBalancesAll) {
-      setFreeKusamaBalance(kusamaBalancesAll.availableBalance);
-    }
-  }, [kusamaBalancesAll]);
-
-  useEffect(() => {
     getUserDeposit && getUserDeposit();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [freeBalance, freeKusamaBalance]);
+  }, [freeBalance]);
 
   return {
-    freeBalance,
-    freeKusamaBalance
+    freeBalance
   };
 };
