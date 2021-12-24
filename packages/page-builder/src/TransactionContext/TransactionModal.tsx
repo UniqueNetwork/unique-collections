3a97a5b3cd8f4f7141cb5ba2821881047b92b04e
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-12-24 18:58:18 +0300

    [NFT-PAR-1016] transactions preloader

--- packages/page-builder/src/TransactionContext/TransactionModal.tsx
+++ packages/page-builder/src/TransactionContext/TransactionModal.tsx
@@ -2,16 +2,44 @@
 // SPDX-License-Identifier: Apache-2.0
 import React from 'react';
 import Modal from 'semantic-ui-react/dist/commonjs/modules/Modal/Modal';
+import Loader from 'semantic-ui-react/dist/commonjs/elements/Loader';
 import styled from 'styled-components';
+import check from '../images/check.svg';
 
-interface Props {
-  closeModal: () => void;
+export type TransactionType = {
+  state: 'not-active' | 'active' | 'finished' | 'error';
+  step: number;
+  text: string;
 }
 
-function TransactionModal ({ closeModal }: Props): React.ReactElement<Props> {
+interface Props {
+  className?: string;
+  closeModal: () => void;
+  transactions: TransactionType[];
+}
+
+const defaultTransactions: TransactionType[] = [
+  {
+    state: 'finished',
+    step: 1,
+    text: 'Setting image location'
+  },
+  {
+    state: 'active',
+    step: 2,
+    text: 'Setting collection trait'
+  },
+  {
+    state: 'not-active',
+    step: 3,
+    text: 'Setting something else'
+  }
+]
+
+function TransactionModal ({ className = '', closeModal, transactions = defaultTransactions }: Props): React.ReactElement<Props> {
   return (
     <Modal
-      className='unique-modal'
+      className={`${className} transition-modal unique-modal`}
       onClose={closeModal}
       open
       size='tiny'
@@ -20,7 +48,46 @@ function TransactionModal ({ closeModal }: Props): React.ReactElement<Props> {
         <h2>Please wait</h2>
       </Modal.Header>
       <Modal.Content>
-        Transaction Modal
+        { transactions.map((transaction) => {
+          if (transaction.state === 'finished') {
+            return (
+              <div className='transition-modal-row with-icon'>
+                <img
+                  alt='check'
+                  src={check as string}
+                />
+                <div>
+                  {transaction.text}
+                  <p>Step {transaction.step}</p>
+                </div>
+              </div>
+            );
+          }
+
+          if (transaction.state === 'active') {
+            return (
+              <div className='transition-modal-row'>
+                <Loader
+                  active
+                  className='load-transaction'
+                >
+                  {transaction.text}
+                  <p>Step {transaction.step}</p>
+                </Loader>
+              </div>
+            )
+          }
+
+          return (
+            <div className='transition-modal-row with-icon'>
+              <div />
+                <div>
+                  {transaction.text}
+                  <p>Step {transaction.step}</p>
+                </div>
+            </div>
+          )
+        })}
       </Modal.Content>
     </Modal>
   );
@@ -28,4 +95,68 @@ function TransactionModal ({ closeModal }: Props): React.ReactElement<Props> {
 
 export default React.memo(styled(TransactionModal)`
   background: #f2f2f2;
+
+  &.transition-modal.unique-modal {
+
+    .content {
+      padding-bottom: calc((var(--gap) / 2) * 3) !important;
+    }
+  }
+
+  .transition-modal-row {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 16px;
+
+    &.with-icon {
+      font-size: 18px;
+      line-height: 26px;
+      display: grid;
+      grid-template-columns: 24px 1fr;
+      grid-column-gap: 16px;
+
+      img {
+        width: 24px;
+      }
+    }
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+
+    p {
+      color: var(--input-placeholder-search-color);
+      font-size: 14px;
+      line-height: 22px;
+      margin: 4px 0 0 0;
+    }
+
+    .load-transaction {
+      &.ui.loader {
+        color: var(--title-color);
+        font-size: 18px;
+        line-height: 26px;
+        display: block;
+        transform: translateX(0%) translateY(0%);
+        left: 0;
+        top: 0;
+
+        &:after {
+          border-color: var(--link-color) transparent transparent;
+          border-width: 2px;
+          left: 0;
+        }
+
+        &:before {
+          border-color: var(--link-color);
+          left: 0;
+        }
+
+        &.text {
+          padding: 0 0 0 40px;
+          text-align: left;
+        }
+      }
+    }
+  }
 `);
