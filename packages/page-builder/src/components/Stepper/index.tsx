10573d4d04af4a3a94ea956e8efd17e9dce52518
pasynkov <n@pasynkov.com>
2022-02-14 13:12:54 +0700

    turn off change steps

--- packages/page-builder/src/components/Stepper/index.tsx
+++ packages/page-builder/src/components/Stepper/index.tsx
@@ -3,49 +3,29 @@
 
 import './styles.scss';
 
-import React, { memo, useCallback } from 'react';
-import { useHistory } from 'react-router';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { memo } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Stepper (): React.ReactElement {
   const location = useLocation();
-  const history = useHistory();
-  const { collectionId }: { collectionId: string } = useParams();
-
-  const onCoverClick = useCallback(() => {
-    if (!location.pathname.includes('/new-collection')) {
-      history.push(`/builder/collections/${collectionId}/cover`);
-    }
-  }, [collectionId, history, location]);
-
-  const onAttributesClick = useCallback(() => {
-    if (!location.pathname.includes('/new-collection')) {
-      history.push(`/builder/collections/${collectionId}/token-attributes`);
-    }
-  }, [collectionId, history, location]);
 
   return (
     <div className='stepper-main shadow-block'>
       <div className='steps'>
         <div
           className={`${location.pathname.includes('/main-information') ? 'step active-step' : 'step'}`}
-          style={{ cursor: 'not-allowed' }}
         >
           1
         </div>
         <div className='step-line' />
         <div
           className={`${location.pathname.includes('/cover') ? 'step active-step' : 'step'}`}
-          onClick={onCoverClick}
-          style={{ cursor: 'pointer' }}
         >
           2
         </div>
         <div className='step-line' />
         <div
           className={`${location.pathname.includes('/token-attributes') ? 'step active-step' : 'step'}`}
-          onClick={onAttributesClick}
-          style={{ cursor: 'pointer' }}
         >
           3
         </div>
